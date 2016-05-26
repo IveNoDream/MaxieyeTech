aa16724f70fa6432967561e9e8fe818c690a59b6
wangfeng <wangfeng@localhost.com>
2016-05-26 16:19:22 +0800

    Build Project

---
+++ MaxieyeTechBT/src/com/maxieyetech/bt/BTTestMainActivity.java
@@ -0,0 +1,13 @@
+package com.maxieyetech.bt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BTTestMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bttest_main);
+	}
+}
